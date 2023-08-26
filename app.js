25b177e5037db4aec68b575e40d45ed9bbe74f6e
andlouro <andremlouro@gmail.com>
2023-08-26 10:28:34 +0100

    Add computer choice

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const gameMove = ["Paper", "Rock", "Scissors"]
+let computerChoice = "";
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * gameMove.length);
+    computerChoice = gameMove[randomNum];
+    console.log(computerChoice);
+}
+
+getComputerChoice()
