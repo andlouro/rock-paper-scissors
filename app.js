8097a3048bc9a818dc71fb6ad8d6316824d1d04a
andlouro <andremlouro@gmail.com>
2023-08-27 11:48:14 +0100

    Add play 5 rounds and result

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const gameMoves = ["Paper", "Rock", "Scissors"]
 let computerSelection = "";
 let playerSelection = "";
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * gameMoves.length);
@@ -11,5 +13,37 @@ function getPlayerSelection() {
     playerSelection = prompt("Type your bet! Available options: Paper or Rock or Scissors").toLowerCase();
 }
 
-getComputerChoice()
-getPlayerSelection()
+function playRound() {
+    if (computerSelection === "paper" && playerSelection === "rock") {
+        computerScore++;
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        computerScore++;
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        computerScore++;
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    } else if (computerSelection === playerSelection) {
+        console.log(`It's a draw! ${computerSelection} equals ${playerSelection}`)
+    } else {
+        playerScore++;
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        getComputerChoice()
+        getPlayerSelection()
+        playRound()
+    }
+    if (computerScore > playerScore) {
+        console.log("Computer Won!")
+    } else if (computerScore === playerScore) {
+        console.log("It's a Draw!")
+    } else {
+        console.log("Well done! You beat the machine")
+    }
+}
+
+game()
